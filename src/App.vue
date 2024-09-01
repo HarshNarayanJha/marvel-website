0f066f5e43556ac17d7c8403e176fcbd11995a3f
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-01 08:23:32 +0530

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <Header />
   </header>
 
   <main>
@@ -20,6 +15,13 @@ import TheWelcome from './components/TheWelcome.vue'
 <style scoped>
 header {
   line-height: 1.5;
+  display: flex;
+  justify-content: space-around;
+  align-items: start;
+  position: sticky;
+  top: 10px;
+
+  margin-bottom: 2em;
 }
 
 .logo {
---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <a href="#"><span class="first">Marvel</span></a>
+    <a href="#features"><span class="">Features</span></a>
+    <a href="#usage"><span class="">Usage</span></a>
+    <a href="#privacy"><span class="">Privacy</span></a>
+    <a href="#about"><span>About</span></a>
+  </nav>
+</template>
+
+<style scoped>
+img.icon {
+  max-width: 30px;
+  margin-top: 7px;
+}
+
+nav {
+  width: fit-content;
+  height: max-content;
+
+  background-color: var(--color-border);
+  /* border: 0.2px solid var(--color-border-hover); */
+
+  border-radius: 40px;
+  padding: 4px;
+
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+
+  span {
+    display: inline-block;
+    width: fit-content;
+    height: max-content;
+
+    margin: 0 5px;
+    padding: 5px 20px;
+    border-radius: 40px;
+
+    color: var(--color-text);
+
+    transition: all 0.5s ease-in-out;
+  }
+
+  span.first {
+    background-color: var(--c-green-dark);
+    font-weight: 900;
+    font-family: "Baloo 2";
+    color: #fff;
+  }
+
+  span.active:not(.first) {
+    background-color: var(--color-border-hover);
+    color: #fff;
+  }
+
+  @media (max-width: 520px) {
+    span {
+      margin: 0 2px;
+      padding: 5px 10px;
+    }
+  }
+}
+
+.links {
+  max-width: 30px;
+  margin-top: 12px;
+}
+
+@media (max-width: 500px) {
+  nav {
+    font-size: 0.8rem;
+    padding: 4px;
+
+    span {
+      padding: 2px 8px;
+      margin: 2px;
+    }
+  }
+}
+</style>
